44a0aabd345b43ab9cb88839f72e8b80ade6f7ae
Michael Placido <mpfx01@gmail.com>
2026-02-09 13:34:19 +0900

    Initial Commit

---
+++ Quiz_Marker_IOS/Quiz_Marker_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_Marker_IOSApp.swift
+//  Quiz_Marker_IOS
+//
+//  Created by Michael Placido on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_Marker_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
